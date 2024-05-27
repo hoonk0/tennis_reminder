291d9ba5567a8880f4791b6105a874204b272f64
hoon <karlhkim1@gmail.com>
2024-05-27 23:03:01 +0900

    -

--- lib/main_screen/my_page/setting/setting_page.dart
+++ lib/main_screen/my_page/setting/setting_page.dart
@@ -1,176 +1,231 @@
 import 'package:flutter/material.dart';
 import 'package:tennisreminder/const/color.dart';
 import 'package:tennisreminder/const/text_style.dart';
+import 'package:tennisreminder/main_screen/my_page/setting/setting_detail/court_manager/setting_manager_court.dart';
+import 'dart:math' as math;
+
+import 'package:tennisreminder/main_screen/my_page/setting/setting_detail/notice/setting_notice.dart';
+import 'package:tennisreminder/main_screen/my_page/setting/setting_detail/setting_contact_manager.dart';
+import '../../../start/login_screen.dart';
 
 class SettingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-
-        body: CustomPaint(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Align(
-              alignment: Alignment.centerLeft,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+        appBar: AppBar(
+          backgroundColor: Color(0xff719a93),
+        ),
+        body: Align(
+          alignment: Alignment.centerLeft,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Column(
+                      children: [
+                        Container(
+                          color: Color(0xff719a93),
+                          height: 80,
+                        ),
+                        Container(
+                          color: Color(0xfff2efef),
+                          height: 50,
+                        ),
+                      ],
+                    ),
+                    Positioned(
+                      child: Container(
+                        width: 100,
+                        height: 100,
+                        decoration: BoxDecoration(
+                          color: Colors.white, // 배경색 흰색
+                          shape: BoxShape.circle,
+                          border: Border.all(
+                            color: Color(0xff719a93),
+                            width: 4,
+                          ),
+                        ),
+                        child: Icon(
+                          Icons.person,
+                          size: 80,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Row(
                 children: [
-
-                  Container(
-                    height: 200,
-                    color: Color(0xfff2efef),
-                  ),
-
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Container(
-                          color: Color(0xfff2efef),
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 20),
-                            child: Row(
-                              children: [
-                                Text(
-                                  'Account',
-                                  style: TS.s24w700(colorGreen900),
-                                ),
-                              ],
+                  Expanded(
+                    child: Container(
+                      color: Color(0xfff2efef),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 20),
+                        child: Row(
+                          children: [
+                            Text(
+                              'Notification',
+                              style: TS.s24w700(colorGreen900),
                             ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const SettingNotice()),
+                  );
+                },
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        color: Color(0xffe8e8e8),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 10),
+                          child: Text(
+                            '공지사항',
+                            style: TS.s20w500(colorGreen900),
                           ),
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
+                ),
+              ),
 
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Container(
-                          color: Color(0xffe8e8e8),
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 10),
-                            child: Row(
-                              children: [
-                                Icon(Icons.contact_support_rounded, color: colorGreen900),
-                                Text(
-                                  '개인정보',
-                                  style: TS.s20w500(colorGreen900),
-                                ),
-                              ],
-                            ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const ContactManager()),
+                  );
+                },
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        color: Color(0xfff2efef),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 10),
+                          child: Text(
+                            '고객센터',
+                            style: TS.s20w500(colorGreen900),
                           ),
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
+                ),
+              ),
 
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Container(
-                          color: Color(0xfff2efef),
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 20),
-                            child: Row(
-                              children: [
-                                Text(
-                                  'Notifications',
-                                  style: TS.s20w500(colorGreen900),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Container(
-                          color: Color(0xffe8e8e8),
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 10),
-                            child: Row(
-                              children: [
-                                Text(
-                                  '공지사항',
-                                  style: TS.s20w500(colorGreen900),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Container(
-                          color: Color(0xfff2efef),
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 10),
-                            child: Row(
-                              children: [
-                                Text(
-                                  '고객센터',
-                                  style: TS.s20w500(colorGreen900),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  
+              Row(
+                children: [
                   Expanded(
                     child: Container(
                       color: Color(0xffe8e8e8),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 20),
+                        child: Row(
+                          children: [
+                            Text(
+                              '\n Account',
+                              style: TS.s24w700(colorGreen900),
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
-                  )
-
+                  ),
                 ],
               ),
-            ),
+
+              GestureDetector(
+
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        color: Color(0xfff2efef),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 10),
+                          child: Text(
+                            '개인정보',
+                            style: TS.s20w500(colorGreen900),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      color: Color(0xffe8e8e8),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 10),
+                        child: Text(
+                          '로그아웃',
+                          style: TS.s20w500(colorGreen900),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const SettingManagerCourt()),
+                  );
+                },
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        color: Color(0xfff2efef),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 10),
+                          child: Row(
+                            children: [
+                              Text(
+                                '관리자 페이지',
+                                style: TS.s20w500(colorGreen900),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              Expanded(
+                child: Container(
+                  color: Color(0xfff2efef),
+                ),
+              ),
+            ],
           ),
         ),
       ),
     );
   }
 }
-
-/*
-class HalfCirclePainter extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    final paint = Paint()
-      ..color = Colors.red // 배경색을 빨간색으로 설정
-      ..style = PaintingStyle.fill;
-
-    final rect = Rect.fromLTWH(
-      0,
-      0,
-      size.width,
-      size.height,
-    );
-
-    final path = Path()
-      ..moveTo(0, size.height)
-      ..lineTo(size.width, size.height)
-      ..arcTo(rect, math.pi, math.pi, false)
-      ..close();
-
-    canvas.drawPath(path, paint);
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return false;
-  }
-}
-*/
